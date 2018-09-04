120bb09dfa5de32d54e6d598ed8bd986acbca479
Akio <akiooarao@gmail.com>
2018-09-04 18:07:35 -0500

    Class Hero
    Added some methods

--- Project/Hero.java
+++ Project/Hero.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Hero extends Character{
 	//Attributes
 	private String name;
@@ -36,5 +38,38 @@ public class Hero extends Character{
 	}
 	public void setEquipment(Item[] equipment){
 		this.equipment = equipment;
+	}
+
+	//Methods
+	public void attack(Enemy enemy){
+		enemy.setHp(enemy.getHp() - getAttack());
+	}
+	public void addItemToBackpack(Item item, int index){	//MAKE SURE IT WORKS
+		backpack[index] = item;
+	}	
+	public void removeItemFromBackpack(int index){ 			//MAKE SURE IT WORKS
+		backpack[index] = null;									//Deletes the object in derised index
+	}
+	public void equipItem(Item item, int index){
+		equipment[index] = item;
+	}
+	public void unequipItem(int index){						//MAKE SURE IT WORKS
+		//Only if backpack isn't full
+		for (int i = 0; i > backpack.length; i++) {			//Go through every slot in the backpack
+			if (backpack[i] == null) { 							//If it finds an empty slot
+				backpack[i] = equipment[i];							//Copy the item from equipment to backback
+				equipment[i] = null;								//Delete item from equipment
+			}
+			if (i == (equipment.length - 1)) {					//If it checks the last slot and is still doesn't find a space to unequip, then show message
+				System.out.println("Backpack is full.");
+			}
+		}
+	}
+	public void useItem(){									//Don't know how to do this yet
+	}
+	public void attackWithAbility(Enemy	enemy, int index){	//Don't know how to do this yet
+	}
+	public boolean scapeAttempt(){							//Won'r do this yet until enemies are programmed
+		Random rand = new Random();
 	}
 }
