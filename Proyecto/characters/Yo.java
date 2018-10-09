08a435e1c4537ae37e9b13bc7b914744520a20c5
Akio <akiooarao@gmail.com>
2018-10-09 10:47:07 -0500

    added packages

--- Proyecto/characters/Yo.java
+++ Proyecto/characters/Yo.java
@@ -1,5 +1,13 @@
 package characters;
 
 public class Yo{
-    
+    //CONSTRUCTOR
+    public Yo(String name, int level, int xp, double hp, double ether, double attack, double defense, boolean statusParalysis, HealingFlask healingFlask){
+        super(name, level, xp, hp, ether, attack, defense, statusParalysis, healingFlask);
+    }
+
+    //METHODS
+    public atacar(Enemigo enemigo){
+        
+    }
 }
