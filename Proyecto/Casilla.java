fed80350075763770018d95347abeffdb9a26cd2
caposcar1998 <42816786+caposcar1998@users.noreply.github.com>
2018-10-09 10:45:32 -0500

    subir casilla
    casilla beta

---
+++ Proyecto/Casilla.java
@@ -0,0 +1,43 @@
+
+
+public class Casilla{
+
+
+
+
+public void setHeroe(Heroe heroe){
+this.heroe=heroe	
+}
+
+public Heroe getHeroe(){
+return heroe;	
+}
+
+
+public void setItem(Item item){
+this.item=item;	
+}//fin set item
+
+public Item getItem(){
+return item;
+}//fin get item
+
+public void setEnemigo(Enemigo enemigo){
+this.enemigo=enemigo;	
+}//fin set enemigo
+
+public Enemigo getEnemigo(){
+return enemigo;	
+}//fin set
+
+public void setRestauracion(){
+this.restauracion=restauracion;	
+}//fin set restauracion
+
+public boolean getRestauracion(boolean restauracion){
+return restauracion;	
+}//fin get restauracion
+
+
+
+}
